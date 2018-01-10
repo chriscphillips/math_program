197c467287bcea9a9082d7f89c7aad24ec0d9e66
chriscphillips <cptp50wv@shentel.net>
2018-01-10 10:22:43 -0500

    addition and substract file

---
+++ math.rb
@@ -0,0 +1,7 @@
+def addition(param1, param2)
+	param1 + param2
+end
+
+def substraction(param1,pram2)
+	param1 -param2
+end
--- test_mathcc.rb
+++ test_mathcc.rb
@@ -1,1 +1,26 @@
+require "minitest/autorun"
+require_relative "math.rb"  #You have to change this what is in quotes to the name of your rb files
 
+class TestAddition < Minitest::Test  #The word Addition can be replace with the name of your project
+
+	def test_assert_that_1_equals_1
+    assert_equal(1,1)
+	end
+
+end
+
+	def test_assert_that_1_equals_1
+    assert_equal(1,1)
+	end
+
+	def test_assert_that_10_plus_3_equal_13
+		assert_equal(10,addition(3))
+	end
+
+	def test_assert_that_100_plus_3_equal_103
+	    assert_equal(100,addition(3))
+	end
+
+	def test_assert_that_100_minus_3_equal_97
+	    assert_equal(100,substraction(3))
+	end
