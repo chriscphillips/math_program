227150108021fa04141fb18514c0d8ee898b11fc
chriscphillips <cptp50wv@shentel.net>
2018-01-10 12:53:56 -0500

    math program cont

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require "sinatra"
+
 get '/' do
-    erb :home	
+    erb :index
 end
+
+post '/math_type' do
+	math_type = params[:math_type]
+	redirect '/math_input?math_answer=' + math_answer
+end
+
+get '/math_input'
+    result == params[:math_answer]
+    if result == "addition"
+         erb :addition
+	elsif result == "substraction" 
+	     erb :substraction
+	elsif result == "divide" 
+		 erb :divde 
+	elsif result == "muplity"
+	else
+	puts "sorry, your answer must be addition, substraction, divide or muplity "	
+end    
